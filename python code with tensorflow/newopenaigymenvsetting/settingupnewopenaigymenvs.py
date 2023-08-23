8e5c495a2a553465b73b145a62b52fcf0a39ab49
WisdomerLee <50577439+WisdomerLee@users.noreply.github.com>
2023-08-23 09:51:34 +0900

    Delete python code with tensorflow directory

--- python code with tensorflow/newopenaigymenvsetting/settingupnewopenaigymenvs.py
+++
@@ -1,8 +0,0 @@
-#python 의 가상 환경을 구축하고.. : 
-#pip install torch==1.13
-#pip install numpy
-#pip install matplotlib
-#pip install gym==0.26.2
-#pip install gym[box2d]
-#pip install miniworld
-#gym의 새 interface와 상호작용하기 위해 Agent를 수정한다면...
